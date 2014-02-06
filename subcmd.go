2d1d93c592717822cd04b3fe83fed310737a2902
Wu, Ke <wuke@cs.umd.edu>
2014-02-06 23:34:24 +0800

    Add simple subcommand support

---
+++ subcmd.go
@@ -0,0 +1,40 @@
+package easy
+
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+type Cmd struct {
+	Help   string
+	Action func(args []string)
+}
+
+func SubCmd(m map[string]Cmd) {
+	prog := os.Args[0]
+	m["help"] = Cmd{"list available subcommands", func(_ []string) { printUsage(prog, m) }}
+	if len(os.Args) <= 1 {
+		printUsage(prog, m)
+		os.Exit(1)
+	}
+	cmd := os.Args[1]
+	sub, ok := m[cmd]
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Unrecognized subcommand: %q\n", cmd)
+		os.Exit(1)
+	}
+	sub.Action(os.Args[2:])
+}
+
+func printUsage(prog string, m map[string]Cmd) {
+	fmt.Fprintf(os.Stderr, "Available subcommands of %s:\n", prog)
+	names := []string{}
+	for name := range m {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		fmt.Fprintf(os.Stderr, "  %s: %s\n", name, m[name].Help)
+	}
+}
